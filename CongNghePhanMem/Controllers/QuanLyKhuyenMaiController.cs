503adddc91cf65b3c766f69470ea69e41b76e97d
hoan <hhrose1307@gmail.com>
2017-12-17 13:17:16 +0700

    Tao khuyen mai trong khuyenmaicontroller

--- CongNghePhanMem/Controllers/QuanLyKhuyenMaiController.cs
+++ CongNghePhanMem/Controllers/QuanLyKhuyenMaiController.cs
@@ -3,11 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CongNghePhanMem.Models;
+using System.IO;
+using PagedList;
+using PagedList.Mvc;
 
 namespace CongNghePhanMem.Controllers
 {
     public class QuanLyKhuyenMaiController : Controller
     {
-        
+        // GET: QuanLyKhuyenMai
+        CongNghePhanMemEntities cn = new CongNghePhanMemEntities();
+        protected void SetAlert(string message, string type)
+        {
+            TempData["AlertMessage"] = message;
+            if (type == "success")
+            {
+                TempData["AlertType"] = "alert-success";
+            }
+            else if (type == "warning")
+            {
+                TempData["AlertType"] = "alert-warning";
+            }
+            else if (type == "error")
+            {
+                TempData["AlertType"] = "alert-danger";
+            }
+        }
+        public ActionResult KhuyenMai(int ?page)
+        {
+            int pageSize = 15;
+            int pageNumber = (page ?? 1);
+            var km = cn.KhuyenMais.ToList().OrderBy(n => n.MaKM).ToPagedList(pageNumber,pageSize);
+            return View(km);
+        }
     }
 }
