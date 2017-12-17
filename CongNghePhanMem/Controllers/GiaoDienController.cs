4ce3e4d8b640ab7f4e03682c4e6e00ac2b4ccc50
Tuan <jt070296@gmail.com>
2017-12-17 21:20:52 +0700

    tao  cac muc trong giaodiencontroller

--- CongNghePhanMem/Controllers/GiaoDienController.cs
+++ CongNghePhanMem/Controllers/GiaoDienController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CongNghePhanMem.Models;
+using PagedList;
+using PagedList.Mvc;
 
 namespace CongNghePhanMem.Controllers
 {
@@ -12,6 +15,32 @@ namespace CongNghePhanMem.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        CongNghePhanMemEntities cn = new CongNghePhanMemEntities();
+        public PartialViewResult Logo()
+        {
+            GiaoDien gd = cn.GiaoDiens.SingleOrDefault(n => n.ID == 1);
+            return PartialView(gd);
+        }
+        public PartialViewResult TieuDe()
+        {
+            GiaoDien tieude = cn.GiaoDiens.SingleOrDefault(n => n.ID == 2);
+            return PartialView(tieude);
+        }
+        public PartialViewResult DienThoai()
+        {
+            GiaoDien dt = cn.GiaoDiens.SingleOrDefault(n => n.ID == 3);
+            return PartialView(dt);
+        }
+        public PartialViewResult DiaChi()
+        {
+            GiaoDien diachi = cn.GiaoDiens.SingleOrDefault(n => n.ID == 4);
+            return PartialView(diachi);
+        }
+        public PartialViewResult FaceBook()
+        {
+            GiaoDien fb = cn.GiaoDiens.SingleOrDefault(n => n.ID == 5);
+            return PartialView(fb);
         }
     }
 }
