d32ab38a0e00ec27e3fe4f9ca55b756e621fee64
hoan <hhrose1307@gmail.com>
2017-12-17 12:15:01 +0700

    Tao setalert va index trong Admincontroller

---
+++ CongNghePhanMem/Controllers/AdminController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CongNghePhanMem.Models;
+using System.IO;
+
+namespace CongNghePhanMem.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        CongNghePhanMemEntities cn = new CongNghePhanMemEntities();
+        protected void SetAlert(string message, string type)
+        {
+            TempData["AlertMessage"] = message;
+            if (type == "success")
+            {
+                TempData["AlertType"] = "alert-success";
+            }
+            else if (type == "warning")
+            {
+                TempData["AlertType"] = "alert-warning";
+            }
+            else if (type == "error")
+            {
+                TempData["AlertType"] = "alert-danger";
+            }
+        }
+        public ActionResult Index()
+        {
+            if (Session["TenDangNhap"] == null || Session["TenDangNhap"].ToString() == "")
+            {
+                return RedirectToAction("LoginAdmin", "Admin");
+            }
+            return View();
+        }
+    }
+}
