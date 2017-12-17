166373d784b1543a7b250f23898c475c0ec8efe6
nguyenvantu439 <nguyenvantu439@gmail.com>
2017-12-17 19:36:43 +0700

    viet code loaisachcontroller

--- CongNghePhanMem/Controllers/LoaiSachController.cs
+++ CongNghePhanMem/Controllers/LoaiSachController.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CongNghePhanMem.Models;
+using PagedList;
+using PagedList.Mvc;
 
 namespace CongNghePhanMem.Controllers
 {
     public class LoaiSachController : Controller
     {
         // GET: LoaiSach
+        CongNghePhanMemEntities cn = new CongNghePhanMemEntities();
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult LoaiSachPartial()
+        {
+            var lst = cn.LoaiSaches.Take(18).ToList();
+            return PartialView(lst);
         }
     }
 }
