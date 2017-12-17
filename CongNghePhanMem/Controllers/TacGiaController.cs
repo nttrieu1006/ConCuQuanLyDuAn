e86e7342b7fe7c9d5b268214450a12ccd88368d5
trieuvy61094@gmail.com <trieuvy61094@gmail.com>
2017-12-17 18:08:36 +0700

    viet code trong tacgiacontroller

--- CongNghePhanMem/Controllers/TacGiaController.cs
+++ CongNghePhanMem/Controllers/TacGiaController.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CongNghePhanMem.Models;
+using PagedList;
+using PagedList.Mvc;
 
 namespace CongNghePhanMem.Controllers
 {
     public class TacGiaController : Controller
     {
-        // GET: TacGia
+        //GET: TacGia
         public ActionResult Index()
         {
             return View();
+        }
+        CongNghePhanMemEntities cn = new CongNghePhanMemEntities();
+        public PartialViewResult TenTacGiaPartial()
+        {
+            var lst = cn.TacGias.ToList();
+            return PartialView(lst);
         }
     }
 }
