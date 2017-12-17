e6c9461d07cdd7a2f3c7653a7efa139c1031b479
hoan <hhrose1307@gmail.com>
2017-12-17 14:14:27 +0700

    XoaSach mua trong QLNhaCungCapcontroller

--- CongNghePhanMem/Controllers/QuanLyNhaCungCapController.cs
+++ CongNghePhanMem/Controllers/QuanLyNhaCungCapController.cs
@@ -116,5 +116,118 @@ namespace CongNghePhanMem.Controllers
             }
             return RedirectToAction("NhaCungCap", "QuanLyNhaCungCap");
         }
+        public ActionResult HopDong(int? page)
+        {
+            int pageSize = 20;
+            int pageNumber = (page ?? 1);
+            var hd = cn.HopDongNCCs.ToList().OrderBy(n => n.MaHD).ToPagedList(pageNumber, pageSize);
+            return View(hd);
+        }
+        [HttpGet]
+        public ActionResult ThemHD()
+        {
+            ViewBag.MaNCC = new SelectList(cn.NhaCungCaps.ToList(), "MaNCC", "TenNCC");
+            return View();
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult ThemHD(HopDongNCC nd)
+        {
+            if (ModelState.IsValid)
+            {
+                HopDongNCC hd = new HopDongNCC();
+                hd.TenHD = nd.TenHD;
+                hd.MaNCC = nd.MaNCC;
+                hd.NgayKi = nd.NgayKi;
+                hd.NgayGiao = nd.NgayGiao;
+                hd.TinhTrangGiaoHang = nd.TinhTrangGiaoHang;
+                hd.TongTien = nd.TongTien;
+                hd.DaThanhToan = nd.DaThanhToan;
+                cn.HopDongNCCs.Add(hd);
+                cn.SaveChanges();
+                SetAlert("Thêm thành công!", "success");
+            }
+            return RedirectToAction("HopDong", "QuanLyNhaCungCap");
+        }
+        //xóa hợp đồng
+        public ActionResult XoaHD(int MaHD = 0)
+        {
+            if (ModelState.IsValid)
+            {
+                ChiTietHopDongMua ct1 = cn.ChiTietHopDongMuas.FirstOrDefault(n => n.MaHD == MaHD);
+
+                if (ct1 != null)
+                {
+                    List<ChiTietHopDongMua> ls = cn.ChiTietHopDongMuas.Where(n => n.MaHD == MaHD).ToList();
+                    for (int i = 0; i < ls.Count; i++)
+                    {
+                        ChiTietHopDongMua ct = cn.ChiTietHopDongMuas.FirstOrDefault(n => n.MaHD == MaHD);
+                        cn.ChiTietHopDongMuas.Remove(ct);
+                        cn.SaveChanges();
+                    }
+                    HopDongNCC hd1 = cn.HopDongNCCs.SingleOrDefault(n => n.MaHD == MaHD);
+                    cn.HopDongNCCs.Remove(hd1);
+                    cn.SaveChanges();
+                    SetAlert("Xóa thành công", "success");
+                }
+                else
+                {
+                    HopDongNCC hd = cn.HopDongNCCs.SingleOrDefault(n => n.MaHD == MaHD);
+                    cn.HopDongNCCs.Remove(hd);
+                    cn.SaveChanges();
+                    SetAlert("Xóa thành công", "success");
+                }
+
+            }
+            return RedirectToAction("HopDong", "QuanLyNhaCungCap");
+        }
+
+        //Show sách mua thuộc hợp đồng
+        public ActionResult Mua(int? page, int MaHD = 0)
+        {
+            int pageSize = 30;
+            int pageNumber = (page ?? 1);
+            var ct = cn.ChiTietHopDongMuas.Where(n => n.MaHD == MaHD).ToList().OrderBy(n => n.STT).ToPagedList(pageNumber, pageSize);
+            Session["MaHD"] = MaHD;
+            return View(ct);
+        }
+        //thêm sách
+
+        [HttpGet]
+        public ActionResult ThemSachMua()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult ThemSachMua(ChiTietHopDongMua ct, int MaHD = 0)
+        {
+            if (ModelState.IsValid)
+            {
+                string hd = Session["MaHD"].ToString();
+                ChiTietHopDongMua ct1 = new ChiTietHopDongMua();
+                ct1.MaHD = int.Parse(hd);
+                ct1.TenSach = ct.TenSach;
+                ct1.SoLuong = ct.SoLuong;
+                ct1.GiaMua = ct.GiaMua;
+                cn.ChiTietHopDongMuas.Add(ct1);
+                cn.SaveChanges();
+                SetAlert("Thêm thành công", "success");
+                var ma = Session["MaHD"];
+            }
+            return RedirectToAction("Mua", "QuanLyNhaCungCap", new { @MaHD = Session["MaHD"] });
+        }
+        public ActionResult XoaSachMua(int STT = 0)
+        {
+            if (ModelState.IsValid)
+            {
+                ChiTietHopDongMua ct = cn.ChiTietHopDongMuas.SingleOrDefault(n => n.STT == STT);
+                cn.ChiTietHopDongMuas.Remove(ct);
+                cn.SaveChanges();
+                SetAlert("Xóa thành công!", "success");
+            }
+            return RedirectToAction("Mua", "QuanLyNhaCungCap", new { @MaHD = Session["MaHD"] });
+
+        }
     }   
 }
