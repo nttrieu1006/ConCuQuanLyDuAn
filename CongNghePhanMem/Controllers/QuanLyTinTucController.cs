08bf679eeb3d359c7b55de3f7f74fef0e365bf1a
Tan Trieu <nttrieu1006@gmail.com>
2017-12-17 12:54:12 +0700

    Tao QuanLyTinTucController

---
+++ CongNghePhanMem/Controllers/QuanLyTinTucController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CongNghePhanMem.Models;
+using System.IO;
+using PagedList;
+using PagedList.Mvc;
+
+
+namespace CongNghePhanMem.Controllers
+{
+    public class QuanLyTinTucController : Controller
+    {
+        CongNghePhanMemEntities cn = new CongNghePhanMemEntities();
+        // GET: QuanLyTinTuc
+        protected void SetAlert(string message, string type)
+        {
+            TempData["AlertMessage"] = message;
+            if (type == "success")
+            {
+                TempData["AlertType"] = "alert-success";
+            }
+            else if (type == "warning")
+            {
+                TempData["AlertType"] = "alert-warning";
+            }
+            else if (type == "error")
+            {
+                TempData["AlertType"] = "alert-danger";
+            }
+        }
+        public ActionResult YKien(int? page)
+        {
+            int pageSize = 15;
+            int pageNumber = (page ?? 1);
+            var pn = cn.PhanHois.Where(n => n.TraLoi == null).ToList().OrderBy(n => n.MaPH).ToPagedList(pageNumber, pageSize);
+            return View(pn);
+        }
+    }
+}
