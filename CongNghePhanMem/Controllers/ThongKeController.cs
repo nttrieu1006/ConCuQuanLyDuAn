34c3351013a98dc5e19e41e6fca80b6d17918f4d
Tan Trieu <nttrieu1006@gmail.com>
2017-12-17 12:44:48 +0700

    Tao ThongKeTheoTheoTg trong ThongKeController

--- CongNghePhanMem/Controllers/ThongKeController.cs
+++ CongNghePhanMem/Controllers/ThongKeController.cs
@@ -15,5 +15,16 @@ namespace CongNghePhanMem.Controllers
         {
             return View();
         }
+        public ActionResult ThongKeDTTheoTG(DateTime? froms, DateTime? tos)
+        {
+            if (froms == null)
+                froms = DateTime.Today.AddMonths(-1);
+            if (tos == null)
+                tos = DateTime.Today;
+            ViewBag.froms = froms.Value.ToShortDateString();
+            ViewBag.tos = tos.Value.ToShortDateString();
+            DonHangKHModel donhang = new DonHangKHModel();
+            return PartialView("TheoTime", donhang.ThongKeDoanhThu(froms, tos).ToList());
+        }
     }
 }
