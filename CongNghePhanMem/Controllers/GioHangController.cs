c9576774fc4f3e6aee5572eafa496bf9fa0d20ea
Tuan <jt070296@gmail.com>
2017-12-17 21:29:21 +0700

    tao setArlet va lay listGio hang trong GiohangController

--- CongNghePhanMem/Controllers/GioHangController.cs
+++ CongNghePhanMem/Controllers/GioHangController.cs
@@ -3,15 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CongNghePhanMem.Models;
+using System.Configuration;
 
 namespace CongNghePhanMem.Controllers
 {
     public class GioHangController : Controller
     {
-        // GET: GioHang
-        public ActionResult Index()
+        //Lấy giỏ hàng
+        CongNghePhanMemEntities cn = new CongNghePhanMemEntities();
+
+        protected void SetAlert(string message, string type)
         {
-            return View();
+            TempData["AlertMessage"] = message;
+            if (type == "success")
+            {
+                TempData["AlertType"] = "alert-success";
+            }
+            else if (type == "warning")
+            {
+                TempData["AlertType"] = "alert-warning";
+            }
+            else if (type == "error")
+            {
+                TempData["AlertType"] = "alert-danger";
+            }
+        }
+        public List<GioHang> LayGioHang()
+        {
+            List<GioHang> lstGioHang = Session["GioHang"] as List<GioHang>;
+            if (lstGioHang == null)
+            {
+                lstGioHang = new List<GioHang>();
+                Session["GioHang"] = lstGioHang;
+
+            }
+            return lstGioHang;
         }
     }
 }
