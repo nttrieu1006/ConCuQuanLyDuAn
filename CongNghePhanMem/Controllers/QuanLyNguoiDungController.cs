8fc9f8a6c8bb5c33489946e90436805b59e76b9a
hoan <hhrose1307@gmail.com>
2017-12-17 13:26:20 +0700

    Tao TinhTrang trong QLNguoidugncontroller

--- CongNghePhanMem/Controllers/QuanLyNguoiDungController.cs
+++ CongNghePhanMem/Controllers/QuanLyNguoiDungController.cs
@@ -3,11 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CongNghePhanMem.Models;
+using PagedList;
+using PagedList.Mvc;
 
 namespace CongNghePhanMem.Controllers
 {
     public class QuanLyNguoiDungController : Controller
     {
-        
+        CongNghePhanMemEntities cn = new CongNghePhanMemEntities();
+        // GET: QuanLyNguoiDung
+        public ActionResult Index()
+        {
+            return View();
+        }
+        protected void SetAlert(string message, string type)
+        {
+            TempData["AlertMessage"] = message;
+            if (type == "success")
+            {
+                TempData["AlertType"] = "alert-success";
+            }
+            else if (type == "warning")
+            {
+                TempData["AlertType"] = "alert-warning";
+            }
+            else if (type == "error")
+            {
+                TempData["AlertType"] = "alert-danger";
+            }
+        }
+
+        public ActionResult TinhTrang()
+        {
+            List<TinhTrangNguoiDung> nguoidung = cn.TinhTrangNguoiDungs.ToList();
+            return View(nguoidung);
+        }
+        [HttpGet]
+        public ActionResult Them()
+        {
+            return View();
+        }
     }
 }
