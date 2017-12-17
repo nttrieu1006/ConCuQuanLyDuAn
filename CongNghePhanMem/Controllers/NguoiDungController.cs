986e02e23fd5f52701fe7e694d206e620cb94468
nguyenvantu439 <nguyenvantu439@gmail.com>
2017-12-17 19:41:10 +0700

    viet code trang nguoidungcontroller

--- CongNghePhanMem/Controllers/NguoiDungController.cs
+++ CongNghePhanMem/Controllers/NguoiDungController.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CongNghePhanMem.Models;
 
 namespace CongNghePhanMem.Controllers
 {
     public class NguoiDungController : Controller
     {
+        CongNghePhanMemEntities cn = new CongNghePhanMemEntities();
         // GET: NguoiDung
-        public ActionResult Index()
+        protected void SetAlert(string message, string type)
         {
-            return View();
+            TempData["AlertMessage"] = message;
+            if (type == "success")
+            {
+                TempData["AlertType"] = "alert-success";
+            }
+            else if (type == "warning")
+            {
+                TempData["AlertType"] = "alert-warning";
+            }
+            else if (type == "error")
+            {
+                TempData["AlertType"] = "alert-danger";
+            }
         }
     }
 }
