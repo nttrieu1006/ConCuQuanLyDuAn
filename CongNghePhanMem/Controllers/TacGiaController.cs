5f9c77e5d8b943a9025cd89b3f65cc6ce05d5f64
trieuvy61094@gmail.com <trieuvy61094@gmail.com>
2017-12-17 18:10:31 +0700

    them code trong tacgiacontroller

--- CongNghePhanMem/Controllers/TacGiaController.cs
+++ CongNghePhanMem/Controllers/TacGiaController.cs
@@ -22,5 +22,23 @@ namespace CongNghePhanMem.Controllers
             var lst = cn.TacGias.ToList();
             return PartialView(lst);
         }
+        public ViewResult SachTheoTacGia(int? page, int MaTG = 0)
+        {
+            int pageSize = 18;
+            int pageNumber = (page ?? 1);
+            TacGia cd = cn.TacGias.SingleOrDefault(n => n.MaTG == MaTG);
+            if (cd == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            var lst = cn.VietSaches.Where(n => n.MaTG == MaTG).ToList().OrderBy(n => n.MaSach).ToPagedList(pageNumber, pageSize);
+            ViewBag.Ten = cn.TacGias.Single(n => n.MaTG == MaTG).TenTG.ToString();
+            if (lst.Count == 0)
+            {
+                ViewBag.Sach = "Không có sách thuộc tác giả này!";
+            }
+            return View(lst);
+        }
     }
 }
